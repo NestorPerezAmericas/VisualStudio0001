ce0fe2158ad4d6615fdfd397c068c86cc86c6cf3
NestorPerezAmericas <Nestor.Perez@americasbps.com>
2017-06-29 12:17:49 -0500

    Varios intentos
    Era un error en la vista del panel por lo que se tuvo que hacer varios intentos que luego se borrarán para no tener embasurado el directorio o carpeta

---
+++ WebApplication001/WebApplication001/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication001.Startup))]
+namespace WebApplication001
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebApplicationNorthWind02/WebApplicationNorthWind02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationNorthWind02.Startup))]
+namespace WebApplicationNorthWind02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
