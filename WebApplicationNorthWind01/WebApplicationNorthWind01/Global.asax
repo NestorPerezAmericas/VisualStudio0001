eb8e776a08b3b5899efaba4c908674bbdc86be76
NestorPerezAmericas <Nestor.Perez@americasbps.com>
2017-06-29 11:43:14 -0500

    Este es el espacio para el Summary
    Apenas se crea nuevo el proyecto. Son los archivos naturales que se crean por defecto. A continuación se creará una rama para mantener original el Master

---
+++ WebApplicationNorthWind01/WebApplicationNorthWind01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationNorthWind01.MvcApplication" Language="C#" %>
---
+++ WebApplicationNorthWind01/WebApplicationNorthWind01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationNorthWind01.Startup))]
+namespace WebApplicationNorthWind01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
